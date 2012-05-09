b268e92b78df9324348b72e0fe49c1c06ec160bf
Ivan Akimov <ivan@grather.com>
2012-05-08 23:30:46 -0700

    better implementation + doc changes

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 
-# hash-ids
+# hashids
 
-A tiny class to generate YouTube-like hashes from one or many ids.
+A tiny PHP class to generate YouTube-like hashes from one or many ids.
 
 ## Contents
 
 * **README.md** - documentation and examples
 * **LICENSE**
 * **lib/**
-	* **hash_ids.php** - main `hash_ids` class for PHP 5.4 and higher
-	* **hash_ids.php-5-3.php** - `hash_ids` class for PHP 5.3 (using regular array notation)
+	* **hashids.php** - main `hashids` class for PHP 5.4 and higher
+	* **hashids.php-5-3.php** - `hashids` class for PHP 5.3 (using regular array notation)
 
 ## What's it for?
 
@@ -19,9 +19,13 @@ Instead of storing these hashes in the database and selecting by them, you could
 
 Hashes look similar to what YouTube, Bitly, and other popular websites have: `p9`, `pZsCB`, `qKuBQuxc`. They are case-sensitive, include alphanumeric characters and a dash.
 
+You can customize the alphabet from which your hashes are created. Simply pass it to the constructor yourself.
+
 ## What's different?
 
 With this class you could encode several ids into one hash. If you have several objects to keep track of, you could use for example `user_id`, `univesity_id` and `class_id` -- passing *all three ids* at the same time and getting back *one hash*.
+
+This is really useful for complex or clustered systems where you need to remember more than one id.
 
 There is no limit to how many ids you can encode into one hash. The more ids you provide and the bigger the numbers, the longer your hash will be.
 
@@ -33,87 +37,98 @@ All integers are expected to be positive.
 
 To encode a single number:
 
-	require_once('lib/hash_ids.php');
-	$hash_ids = new hash_ids('this is my salt');
-	
-	$hash = $hash_ids->encode(12345);
-	
+```php
+require_once('lib/hashids.php');
+$hashids = new hashids('this is my salt');
+
+$hash = $hashids->encode(12345);
+```
+
 `$hash` is now going to be:
-
+	
 	7OR
-
+	
 To encode multiple numbers into one hash:
 
-	require_once('lib/hash_ids.php');
-	$hash_ids = new hash_ids('this is my salt');
-	
-	$hash = $hash_ids->encode(683, 94108, 123, 5);
-	
+```php
+require_once('lib/hashids.php');
+$hashids = new hashids('this is my salt');
+
+$hash = $hashids->encode(683, 94108, 123, 5);
+```
+
 `$hash` is now going to be:
 
-	nEFOM6s7wI6
-	
+	nEfOM6s2oIz
 
 ### Decoding:
 
 Hash decoding is done using the same salt value:
 
-	require_once('lib/hash_ids.php');
-	$hash_ids = new hash_ids('this is my salt');
-	
-	$first_hash = $hash_ids->decode('7OR');
-	var_dump($first_hash);
-	
-	$second_hash = $hash_ids->decode('nEFOM6s7wI6');
-	var_dump($second_hash);
-	
+```php
+require_once('lib/hashids.php');
+$hashids = new hashids('this is my salt');
+
+$first_hash = $hashids->decode('7OR');
+var_dump($first_hash);
+
+$second_hash = $hashids->decode('nEfOM6s2oIz');
+var_dump($second_hash);
+```
+
 Output will be:
 
-	array(1) {
-		[0]=>
-		int(12345)
-	}
-	array(4) {
-		[0]=>
-		int(683)
-		[1]=>
-		int(94108)
-		[2]=>
-		int(123)
-		[3]=>
-		int(5)
-	}
+```php
+array(1) {
+  [0]=>
+  int(12345)
+}
+array(4) {
+  [0]=>
+  int(683)
+  [1]=>
+  int(94108)
+  [2]=>
+  int(123)
+  [3]=>
+  int(5)
+}
+```
 
 ## Security
 
-The primary purpose of this hash function is to make ids look different. It's not meant or tested to be used primarily as a security algorithm.
+The primary purpose of this algorithm is to make ids look different. It's not meant or tested to be used as a security algorithm.
 
 Having said that, this class does try to make these hashes un-guessable and unique.
 
-Let's for example look at the following code:
+Let's look at the following example:
 
-	require_once('lib/hash_ids.php');
-	$hash_ids = new hash_ids('this is my salt');
-	
-	$hash = $hash_ids->encode(5, 5, 5, 5);
-	
+```php
+require_once('lib/hashids.php');
+$hashids = new hashids('this is my salt');
+
+$hash = $hashids->encode(5, 5, 5, 5);
+```
+
 `$hash` will be:
 
-	jief5sd
+	jie1ws6
 	
 You don't see any repeating patterns that might show there's 4 identical numbers in the hash.
 
 Same with incremented numbers:
 
-	require_once('lib/hash_ids.php');
-	$hash_ids = new hash_ids('this is my salt');
-	
-	$hash = $hash_ids->encode(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-	
+```php
+require_once('lib/hashids.php');
+$hashids = new hashids('this is my salt');
+
+$hash = $hashids->encode(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+```
+
 `$hash` will be :
-
-	6uts5Iaf2s7hjSw16ho
-
+	
+	6utsaI616snh0SdFthj
+	
 ## Bonus
 
-Since these hashes are most likely to be used in user-visible places, like the url -- no matter the salt value, they will not make up basic curse words by design, like the f-bomb or "#2".
+Since these hashes are most likely to be used in user-visible places, like the url -- no matter the salt value, they should not make up basic curse words by design, like the f-bomb or "#2".
--- lib/hashids.php
+++ lib/hashids.php
@@ -1,168 +1,186 @@
 <?php
+
+// hashids 0.1.1
+// (c) 2012 Ivan Akimov
+// https://github.com/ivanakimov/hashids.php
+// hashids may be freely distributed under the MIT license.
+
+class hashids {
 	
-	// hashids 0.1.0
-	// (c) 2012 Ivan Akimov
-	// https://github.com/ivanakimov/hash-ids
-	// hash-ids may be freely distributed under the MIT license.
+	private $salt;
+	private $alphabet = '-023456789abdegjklmnopqrtvwxyzABDEGJKLMNOPQRTVWXYZ';
+	private $separators = '1fFuUsSiIcChH';
 	
-	class hashids {
+	function __construct($salt = '', $alphabet = '', $separators = '') {
 		
-		private $salt;
-		private $alphabet = '-023456789abdegjklmnopqrtvwxyzABDEGJKLMNOPQRTVWXYZ';
-		private $separators = '1fFuUsSiIcChH';
+		$this->salt = $salt;
 		
-		function __construct($salt = '') {
-			
-			$this->salt = $salt;
-			$this->alphabet_length = strlen($this->alphabet);
-			
-			if (strlen($this->salt))
-				$this->alphabet = $this->_shuffle($this->alphabet, $this->salt);
-			
-		}
+		if ($alphabet)
+			$this->alphabet = $alphabet;
 		
-		function encode() {
-			return call_user_func_array([$this, '_encode'], func_get_args());
-		}
+		if ($separators)
+			$this->separators = $separators;
 		
-		function decode($hash) {
-			return $this->_decode($hash);
-		}
+		$this->alphabet_length = strlen($this->alphabet);
 		
-		private function _encode() {
-			
-			$ret = '';
-			$alphabet = $this->alphabet;
-			
-			$args = func_get_args();
-			foreach ($args as $i => $arg) {
-				
-				if ($arg < 0) {
-					$ret = '';
-					break;
-				}
-				
-				if ($i) {
-					$params = array_slice($args, 0, $i);
-					$ret .= call_user_func_array([$this, '_get_separator'], $params);
-				}
-				
-				$hash = $this->_hash($arg, $alphabet);
-				$ret .= $hash;
-				
-				$alphabet = $this->_shuffle($alphabet, $this->salt . $hash);
-				
-			}
-			
-			return $ret;
-			
-		}
+		if (strlen($this->salt))
+			$this->alphabet = $this->_shuffle($this->alphabet, $this->salt);
 		
-		private function _decode($hash) {
-			
-			$ret = [];
-			$alphabet = $this->alphabet;
-			
-			$hash = trim($hash);
-			if ($hash) {
-				
-				$separator_array = str_split($this->separators);
-				$hash_spaces = str_replace($separator_array, ' ', $hash);
-				$hash_array = explode(' ', $hash_spaces);
-				
-				foreach ($hash_array as $i => $sub_hash) {
-					
-					if ($sub_hash) {
-						
-						$id = $this->_unhash($sub_hash, $alphabet);
-						$ret[] = $id;
-						
-						if ($i + 1 < sizeof($hash_array))
-							$alphabet = $this->_shuffle($alphabet, $this->salt . $sub_hash);
-						
-					}
-					
-				}
-				
-			}
-			
-			$validate = call_user_func_array([$this, 'encode'], $ret);
-			if ($validate != $hash)
-				$ret = [];
-			
-			return $ret;
-			
-		}
+		/* check that alphabet is unique */
 		
-		private function _shuffle($alphabet, $salt) {
-			
-			$ret = '';
-			$alphabet_array = str_split($this->alphabet);
-			$sorting_array = str_split(md5($salt));
-			
-			$i = 0;
-			while ($alphabet_array) {
-				
-				$alphabet_size = sizeof($alphabet_array);
-				$pos = hexdec($sorting_array[$i]);
-				
-				if ($pos >= $alphabet_size)
-					$pos = ($alphabet_size - 1) % $pos;
-				
-				$ret .= $alphabet_array[$pos];
-				unset($alphabet_array[$pos]);
-				$alphabet_array = array_merge($alphabet_array);
-				
-				$i++;
-				$i %= sizeof($sorting_array);
-				
-			}
-			
-			return $ret;
-			
-		}
+		$alphabet_exploded = str_split($this->alphabet);
+		if (strlen($this->alphabet) != sizeof(array_unique($alphabet_exploded)))
+			trigger_error('Alphabet contains duplicate characters', E_USER_WARNING);
 		
-		private function _hash($input, $alphabet) {
-			
-			$ret = '';
-			$alphabet_length = strlen($alphabet);
-			
-			do {
-				$rem = $input % $alphabet_length;
-				$ret = $alphabet[$rem] . $ret;
-			} while ((int)$input = $input / $alphabet_length);
-			
-			return $ret;
-			
-		}
+		/* check that separators are unique */
 		
-		private function _unhash($input, $alphabet) {
-			
-			$ret = 0;
-			$alphabet_length = strlen($alphabet);
-			$input_chars = str_split($input);
-			
-			foreach ($input_chars as $i => $char) {
-				$pos = strpos($alphabet, $char);
-				$ret += $pos * pow($alphabet_length, (strlen($input) - $i - 1));
-			}
-			
-			return $ret;
-			
-		}
+		$separators_exploded = str_split($this->separators);
+		if (strlen($this->separators) != sizeof(array_unique($separators_exploded)))
+			trigger_error('Separator string contains duplicate characters', E_USER_WARNING);
 		
-		private function _get_separator() {
-			
-			$args = func_get_args();
-			$sum = $this->alphabet_length;
-			
-			foreach ($args as $arg)
-				$sum += $arg;
-			
-			$i = $sum % (strlen($this->separators) - 1);
-			return $this->separators[$i];
-			
-		}
+		/* check that separators do not exist in alphabet */
+		
+		if (array_intersect($alphabet_exploded, $separators_exploded))
+			trigger_error('Separator characters cannot be part of the alphabet', E_USER_WARNING);
 		
 	}
 	
+	function encode() {
+		return call_user_func_array([$this, '_encode'], func_get_args());
+	}
+	
+	function decode($hash) {
+		return $this->_decode($hash);
+	}
+	
+	private function _encode() {
+		
+		$ret = '';
+		$alphabet = $this->alphabet;
+		
+		$args = func_get_args();
+		foreach ($args as $i => $arg) {
+			
+			if ($arg < 0) {
+				$ret = '';
+				break;
+			}
+			
+			if ($i) {
+				$params = array_slice($args, 0, $i);
+				$ret .= call_user_func_array([$this, '_get_separator'], $params);
+			}
+			
+			$hash = $this->_hash($arg, $alphabet);
+			$ret .= $hash;
+			
+			$alphabet = $this->_shuffle($alphabet, $this->salt . $hash);
+			
+		}
+		
+		return $ret;
+		
+	}
+	
+	private function _decode($hash) {
+		
+		$ret = [];
+		$alphabet = $this->alphabet;
+		
+		$hash = trim($hash);
+		if ($hash) {
+			
+			$separator_array = str_split($this->separators);
+			$hash_spaces = str_replace($separator_array, ' ', $hash);
+			$hash_array = explode(' ', $hash_spaces);
+			
+			foreach ($hash_array as $i => $sub_hash) {
+				
+				$ret[] = $this->_unhash($sub_hash, $alphabet);
+				
+				if ($i + 1 < sizeof($hash_array))
+					$alphabet = $this->_shuffle($alphabet, $this->salt . $sub_hash);
+				
+			}
+			
+		}
+		
+		$validate = call_user_func_array([$this, 'encode'], $ret);
+		if ($validate != $hash)
+			$ret = [];
+		
+		return $ret;
+		
+	}
+	
+	private function _shuffle($alphabet, $salt) {
+		
+		$ret = '';
+		$alphabet_array = str_split($alphabet);
+		$sorting_array = str_split(md5($salt));
+		
+		$i = 0;
+		while ($alphabet_array) {
+			
+			$alphabet_size = sizeof($alphabet_array);
+			$pos = hexdec($sorting_array[$i]);
+			
+			if ($pos >= $alphabet_size)
+				$pos = ($alphabet_size - 1) % $pos;
+			
+			$ret .= $alphabet_array[$pos];
+			unset($alphabet_array[$pos]);
+			$alphabet_array = array_merge($alphabet_array);
+			
+			$i++;
+			$i %= sizeof($sorting_array);
+			
+		}
+		
+		return $ret;
+		
+	}
+	
+	private function _hash($input, $alphabet) {
+		
+		$ret = '';
+		$alphabet_length = strlen($alphabet);
+		
+		do {
+			$rem = $input % $alphabet_length;
+			$ret = $alphabet[$rem] . $ret;
+		} while ((int)$input = $input / $alphabet_length);
+		
+		return $ret;
+		
+	}
+	
+	private function _unhash($input, $alphabet) {
+		
+		$ret = 0;
+		$alphabet_length = strlen($alphabet);
+		$input_chars = str_split($input);
+		
+		foreach ($input_chars as $i => $char) {
+			$pos = strpos($alphabet, $char);
+			$ret += $pos * pow($alphabet_length, (strlen($input) - $i - 1));
+		}
+		
+		return $ret;
+		
+	}
+	
+	private function _get_separator() {
+		
+		$args = func_get_args();
+		$sum = $this->alphabet_length;
+		
+		foreach ($args as $arg)
+			$sum += $arg;
+		
+		$i = $sum % (strlen($this->separators) - 1);
+		return $this->separators[$i];
+		
+	}
+	
+}
