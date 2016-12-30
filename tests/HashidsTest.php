e2104ae9f8bfaf19e797b8facfbf7994a5098c03
Jakub Kramarz <jakub.kramarz@freshmail.pl>
2016-12-30 23:32:57 +0100

    set number of digits after the decimal place in the result of bc math functions

--- tests/HashidsTest.php
+++ tests/HashidsTest.php
@@ -280,4 +280,14 @@ class HashidsTest extends AbstractTestCase
         $decoded = $hashids->decode($hash);
         $this->assertEquals($number, $decoded[0]);
     }
+
+    public function testBehaviourForDifferentBCMathAccuracy()
+    {
+        if (function_exists('bcscale')) {
+            bcscale(2);
+            $hashids = new Hashids('this is my salt', 12);
+            $encoded = $hashids->encode(1);
+            $this->assertEquals('DngB0NV05ev1', $encoded);
+        }
+    }
 }
