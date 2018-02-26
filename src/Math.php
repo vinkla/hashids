355230381a44284e546ca29f998ed05f07cf1395
Johnson Page <jwpage@gmail.com>
2018-02-26 11:18:07 +1100

    Update Math to use MathFactory

--- src/Math.php
+++ src/Math.php
@@ -11,6 +11,8 @@
 
 namespace Hashids;
 
+use Hashids\Math\MathInterface;
+use Hashids\Math\MathFactory;
 use RuntimeException;
 
 /**
@@ -19,168 +21,60 @@ use RuntimeException;
  * @author Vincent Klaiber <hello@vinkla.com>
  * @author Jakub Kramarz <lenwe@lenwe.net>
  */
-class Math
+class Math implements MathInterface
 {
     /**
-     * Add two arbitrary-length integers.
-     *
-     * @param string $a
-     * @param string $b
-     *
      * @throws \RuntimeException
-     *
-     * @return string
      */
     public static function add($a, $b)
     {
-        if (function_exists('gmp_add')) {
-            return gmp_add($a, $b);
-        }
-
-        if (function_exists('bcadd')) {
-            return bcadd($a, $b, 0);
-        }
-
-        throw new RuntimeException('Missing BC Math or GMP extension.');
+        return MathFactory::create()->add($a, $b);
     }
 
     /**
-     * Multiply two arbitrary-length integers.
-     *
-     * @param string $a
-     * @param string $b
-     *
      * @throws \RuntimeException
-     *
-     * @return string
      */
     public static function multiply($a, $b)
     {
-        if (function_exists('gmp_mul')) {
-            return gmp_mul($a, $b);
-        }
-
-        if (function_exists('bcmul')) {
-            return bcmul($a, $b, 0);
-        }
-
-        throw new RuntimeException('Missing BC Math or GMP extension.');
+        return MathFactory::create()->multiply($a, $b);
     }
 
     /**
-     * Divide two arbitrary-length integers.
-     *
-     * @param string $a
-     * @param string $b
-     *
      * @throws \RuntimeException
-     *
-     * @return string
      */
     public static function divide($a, $b)
     {
-        if (function_exists('gmp_div_q')) {
-            return gmp_div_q($a, $b);
-        }
-
-        if (function_exists('bcdiv')) {
-            return bcdiv($a, $b, 0);
-        }
-
-        throw new RuntimeException('Missing BC Math or GMP extension.');
+        return MathFactory::create()->divide($a, $b);
     }
 
     /**
-     * Compute arbitrary-length integer modulo.
-     *
-     * @param string $n
-     * @param string $d
-     *
      * @throws \RuntimeException
-     *
-     * @return string
      */
     public static function mod($n, $d)
     {
-        if (function_exists('gmp_mod')) {
-            return gmp_mod($n, $d);
-        }
-
-        if (function_exists('bcmod')) {
-            return bcmod($n, $d);
-        }
-
-        throw new RuntimeException('Missing BC Math or GMP extension.');
+        return MathFactory::create()->mod($n, $d);
     }
 
     /**
-     * Compares two arbitrary-length integers.
-     *
-     * @param string $a
-     * @param string $b
-     *
      * @throws \RuntimeException
-     *
-     * @return bool
      */
     public static function greaterThan($a, $b)
     {
-        if (function_exists('gmp_cmp')) {
-            return gmp_cmp($a, $b) > 0;
-        }
-
-        if (function_exists('bccomp')) {
-            return bccomp($a, $b, 0) > 0;
-        }
-
-        throw new RuntimeException('Missing BC Math or GMP extension.');
+        return MathFactory::create()->greaterThan($a, $b);
     }
 
-    /**
-     * Converts arbitrary-length integer to PHP integer.
-     *
-     * @param string $a
-     *
-     * @return int
-     */
     public static function intval($a)
     {
-        if (function_exists('gmp_intval')) {
-            return gmp_intval($a);
-        }
-
-        return intval($a);
+        return MathFactory::create()->intval($a);
     }
 
-    /**
-     * Converts arbitrary-length integer to PHP string.
-     *
-     * @param string $a
-     *
-     * @return string
-     */
     public static function strval($a)
     {
-        if (function_exists('gmp_strval')) {
-            return gmp_strval($a);
-        }
-
-        return $a;
+        return MathFactory::create()->strval($a);
     }
 
-    /**
-     * Converts PHP integer to arbitrary-length integer.
-     *
-     * @param int $a
-     *
-     * @return string
-     */
     public static function get($a)
     {
-        if (function_exists('gmp_init')) {
-            return gmp_init($a);
-        }
-
-        return $a;
+        return MathFactory::create()->get($a);
     }
 }
